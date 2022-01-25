0f53c9cf78f1a41c907dc32d369740f47084f1a2
Elena Voytovich <elena-voytovich@yandex.ru>
2022-01-25 14:31:55 +0300

    refactor: code refactoring

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -8,7 +8,7 @@ const Article = ({data}) => {
       {data.map((item) => {
         const {id, name, image, alt} = item;
         return (
-          <article key={id} className='section__article article'>
+          <article key={id} className='article'>
             <img className='article__img' src={image} alt={alt}/>
             <h4 className='article__title'>{name}</h4>
             <button className='article__btn' type='button'>Learn more</button>

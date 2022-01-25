5fa5ba960626ac00a72319c53ade3408353f269f
Elena Voytovich <elena-voytovich@yandex.ru>
2022-01-25 14:29:23 +0300

    refactor: code refactoring

---
+++ src/components/Article/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Article.scss';
+
+const Article = ({data}) => {
+  return (
+    <>
+      {data.map((item) => {
+        const {id, name, image, alt} = item;
+        return (
+          <article key={id} className='section__article article'>
+            <img className='article__img' src={image} alt={alt}/>
+            <h4 className='article__title'>{name}</h4>
+            <button className='article__btn' type='button'>Learn more</button>
+          </article>
+        )
+      })}
+    </>
+  );
+};
+
+export default Article;
---
+++ src/data.js
@@ -0,0 +1,61 @@
+import photoKatrine from './assets/img/pets-katrine.png';
+import photoJennifer from './assets/img/pets-jennifer.png';
+import photoWoody from './assets/img/pets-woody.png';
+import photoSophia from './assets/img/pets-sophia.png';
+import photoTimmy from './assets/img/pets-timmy.png';
+import photoCharly from './assets/img/pets-charly.png';
+import photoScarlett from './assets/img/pets-scarlett.png';
+import photoFreddie from './assets/img/pets-freddie.png';
+
+const data = [
+  {
+    id: 1,
+    name: 'Katrine',
+    image: photoKatrine,
+    alt: 'photo-Katrine'
+  },
+  {
+    id: 2,
+    name: 'Jennifer',
+    image: photoJennifer,
+    alt: 'photo-Jennifer'
+  },
+  {
+    id: 3,
+    name: 'Woody',
+    image: photoWoody,
+    alt: 'photo-Woody'
+  },
+  {
+    id: 4,
+    name: 'Sophia',
+    image: photoSophia,
+    alt: 'photo-Sophia'
+  },
+  {
+    id: 5,
+    name: 'Timmy',
+    image: photoTimmy,
+    alt: 'photo-Timmy'
+  },
+  {
+    id: 6,
+    name: 'Charly',
+    image: photoCharly,
+    alt: 'photo-Charly'
+  },
+  {
+    id: 7,
+    name: 'Scarlett',
+    image: photoScarlett,
+    alt: 'photo-Scarlett'
+  },
+  {
+    id: 8,
+    name: 'Freddie',
+    image: photoFreddie,
+    alt: 'photo-Freddie'
+  },
+];
+
+export default data;
